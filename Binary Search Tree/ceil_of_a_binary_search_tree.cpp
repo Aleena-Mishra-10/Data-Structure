428b98563de2610edd218fcf38a68a121f276c0f
Aleena MIshra <56436393+Aleena-Mishra-10@users.noreply.github.com>
2020-08-23 08:30:58 +0530

    Add files via upload

---
+++ Binary Search Tree/ceil_of_a_binary_search_tree.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+	int data;
+	struct node *left;
+	struct node *right;
+};
+struct node *root=NULL;
+void create_bst(int n);
+void preorder(struct node *root);
+struct node *find_ceil(int key);
+
+void create_bst(int n){
+	struct node *ptr;
+	struct node *child;
+	struct node *new_node = (struct node *)malloc(sizeof(struct node));
+	new_node->data=n;
+	new_node->left=NULL;
+	new_node->right=NULL;
+	if(root==NULL){
+		root=new_node;
+		root->left=NULL;
+		root->right=NULL;
+	}
+	else{
+		ptr=NULL;
+		child=root;
+		while(child!=NULL){
+			ptr=child;
+			if(child->data>n){
+				child=child->left;
+			}
+			else{
+				child=child->right;
+			}
+		}
+		if(n<ptr->data){
+			ptr->left=new_node;
+		}
+		else{
+			ptr->right=new_node;
+		}
+	}
+}
+void preorder(struct node *root){
+	if(root==NULL){
+		return ;
+	}
+	printf("%d\n",root->data);
+	preorder(root->left);
+	preorder(root->right);
+}
+
+struct node *find_ceil(int key){
+	struct node *res;
+	struct node *t=root;
+	while(t!=NULL){
+		if(t->data<key){
+			t=t->right;
+		}
+		else{
+			res=t;
+			t=t->left;
+		}	
+	}
+	return res;
+}
+int main(){
+	int val,key;
+	char ch;
+	struct node *t;
+	do{
+		cout<<"Enter an element:\n";
+		cin>>val;
+		create_bst(val);
+		cout<<"Do you want to enter more?\n";
+		cin>>ch;
+	}while(ch=='y');
+	preorder(root);
+	cout<<"Enter an element whose ceil you want to find\n";
+	cin>>key;
+	t=find_ceil(key);
+	cout<<"Ceil is:\t"<<t->data;
+	return 0;
+}
