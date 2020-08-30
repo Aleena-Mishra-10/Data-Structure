21feaef35d294a2eb991737b7c9c1664b49af575
Aleena MIshra <56436393+Aleena-Mishra-10@users.noreply.github.com>
2020-08-30 11:05:02 +0530

    Add files via upload

---
+++ Bit Magic/even_xor.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+bool isEven(int n){
+    
+    if(n^1==n+1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int main(){
+       
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+    isEven(n)? cout<<"Even":cout<<"Odd";
+    return 0;
+}
