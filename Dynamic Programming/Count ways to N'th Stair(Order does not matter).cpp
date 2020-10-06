4ad15327cfbd6541fda9c9f24e599bdd110bf4af
Aleena MIshra <56436393+Aleena-Mishra-10@users.noreply.github.com>
2020-10-06 20:06:50 +0530

    Add files via upload

---
+++ Dynamic Programming/Count ways to N'th Stair(Order does not matter).cpp
@@ -0,0 +1,14 @@
+long long countWays(int m){
+    long long dp[m+1];
+    dp[0]=1;
+    for(int i=1;i<=m;i++){
+        dp[i]=0;
+    }
+    for(int i=1;i<=m;i++){
+        dp[i]+=dp[i-1];
+    }
+    for(int i=2;i<=m;i++){
+        dp[i]+=dp[i-2];
+    }
+    return dp[m];
+}
