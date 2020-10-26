0c434bf4c200a0a57b71876da463fa12500f844b
Aleena MIshra <56436393+Aleena-Mishra-10@users.noreply.github.com>
2020-10-26 19:34:04 +0530

    Add files via upload

---
+++ Dynamic Programming/nCr.cpp
@@ -0,0 +1,23 @@
+int nCrModp(int n, int r) 
+{ 
+  
+    long long dp[n+1][n+1];
+    if(r>n){
+        return 0;
+    }
+    dp[0][0]=1;
+    int i,j;
+    for(i=1;i<=n;i++){
+        for(j=0;j<=i;j++){
+            dp[i][j]=0;
+            if(j>=0 && j<i){
+                dp[i][j]=(dp[i][j]+dp[i-1][j])%p;
+            }
+            if(j-1 >= 0 && j-1 < i)
+            {
+                dp[i][j] = (dp[i][j] + dp[i-1][j-1])%p;
+            }
+        }
+    }
+  return dp[n][r];
+} 
