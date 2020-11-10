5cf50d616d11ac2f0eae40da0848972ec6b36f28
Aleena MIshra <56436393+Aleena-Mishra-10@users.noreply.github.com>
2020-11-10 09:59:18 +0530

    Add files via upload

---
+++ LeetCode/Maximum Difference Between Node and Ancestor .cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int helper(TreeNode* root,int curmin,int curmax)
+{
+    if(root==NULL)
+    {
+        return curmax-curmin;
+    }
+    
+    curmax=max(root->val,curmax);
+    curmin=min(root->val,curmin);
+    
+    int l=helper(root->left,curmin,curmax);
+    int r=helper(root->right,curmin,curmax);
+    
+    return max(l,r);
+}
+    int maxAncestorDiff(TreeNode* root) {
+      if(root==NULL)
+    {
+        return 0;
+    }
+    
+    return helper(root,root->val,root->val);
+       
+    }
+};
